b346f0551244d4a0641e77ab1eba261c5659d872
LevPavelko <lev.pavelko.studies@gmail.com>
2024-04-08 12:00:54 +0200

    Add project files.

---
+++ SignalR_Chat/Program.cs
@@ -0,0 +1,16 @@
+using SignalR_Chat;   // пространство имен класса ChatHub
+
+var builder = WebApplication.CreateBuilder(args);
+// ƒл€ использовани€ функциональности библиотеки SignalR,
+// в приложении необходимо зарегистрировать соответствующие сервисы
+builder.Services.AddSignalR();  
+
+var app = builder.Build();
+// — помощью специального метода расширени€ UseDefaultFiles() можно настроить
+// отправку статических веб-страниц по умолчанию без обращени€ к ним по полному пути.
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/chat");   // ChatHub будет обрабатывать запросы по пути /chat
+
+app.Run();
