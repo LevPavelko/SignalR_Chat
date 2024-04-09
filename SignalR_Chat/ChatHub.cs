24bf1e5b2de70f30abb7766fe56e0b11f47ecbdf
LevPavelko <lev.pavelko.studies@gmail.com>
2024-04-09 09:43:18 +0200

    Task2: Исправление ошибок с выводом времени отправки сообщения

--- SignalR_Chat/ChatHub.cs
+++ SignalR_Chat/ChatHub.cs
@@ -3,12 +3,7 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace SignalR_Chat
 {
-    /*
-    Ключевой сущностью в SignalR, через которую клиенты обмениваются сообщеними 
-    с сервером и между собой, является хаб (hub). 
-    Хаб представляет некоторый класс, который унаследован от абстрактного класса 
-    Microsoft.AspNetCore.SignalR.Hub.
-    */
+    
     public class ChatHub : Hub
     {
         private ChatContext db;
@@ -19,8 +14,8 @@ namespace SignalR_Chat
             db = context;
         }
 
-        // Отправка сообщений
-        public async Task Send(string username, string message, DateTime date)
+        
+        public async Task Send(string username, string message, string date)
         {
             
            
@@ -36,7 +31,6 @@ namespace SignalR_Chat
             await Clients.All.SendAsync("AddMessage", username, message,date);
         }
 
-        // Подключение нового пользователя
         public async Task Connect(string userName)
         {
             var id = Context.ConnectionId;
@@ -48,10 +42,10 @@ namespace SignalR_Chat
 
               
 
-                // Вызов метода Connected только на текущем клиенте, который обратился к серверу
+                
                 await Clients.Caller.SendAsync("Connected", id, userName, Users, history);
 
-                // Вызов метода NewUserConnected на всех клиентах, кроме клиента с определенным id
+              
                 await Clients.AllExcept(id).SendAsync("NewUserConnected", id, userName);
             }
         }
@@ -64,9 +58,7 @@ namespace SignalR_Chat
             await Clients.Caller.SendAsync("ShowHistory", history);
         }
 
-        // OnDisconnectedAsync срабатывает при отключении клиента.
-        // В качестве параметра передается сообщение об ошибке, которая описывает,
-        // почему произошло отключение.
+       
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
             var item = Users.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
@@ -74,7 +66,7 @@ namespace SignalR_Chat
             {
                 Users.Remove(item);
                 var id = Context.ConnectionId;
-                // Вызов метода UserDisconnected на всех клиентах
+               
                 await Clients.All.SendAsync("UserDisconnected", id, item.Name);
             }
             await base.OnDisconnectedAsync(exception);
